231752e113156cdc0d6e0688f6d68081c4aba3e7
Maria <mariajoseguilarte@gmail.com>
2019-02-17 10:31:09 -0430

    Return collections for view

--- app/Http/Controllers/DevelopmentStatusController.php
+++ app/Http/Controllers/DevelopmentStatusController.php
@@ -10,11 +10,13 @@ class DevelopmentStatusController extends Controller
     public function index()
     {
       $dev_statuses = DevelopmentStatus::all();
+      return view('development_statuses.index', compact($dev_statuses));
+      // return $dev_statuses;
     }
     
     public function create()
     {
-      
+      return view('developmentStatuses.create');
     }
 
     public function store(Request $request)
@@ -26,12 +28,13 @@ class DevelopmentStatusController extends Controller
 
     public function show(DevelopmentStatus $developmentStatus)
     {
-        //
+      return view('development_statuses.show', compact($developmentStatus));
+      //return $developmentStatus;
     }
 
     public function edit(DevelopmentStatus $developmentStatus)
     {
-        //
+      return view('development_statuses.edit', compact($developmentStatus));
     }
 
     public function update(Request $request, DevelopmentStatus $developmentStatus)
